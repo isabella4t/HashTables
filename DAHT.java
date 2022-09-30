4042b3bc108b2aa0174e351ab94fd3d84fd9211c
isabella4t <70291996+isabella4t@users.noreply.github.com>
2022-09-29 23:04:41 -0700

    Update DAHT.java

--- DAHT.java
+++ DAHT.java
@@ -1,82 +1,48 @@
-import java.io.*;
 import java.util.Arrays;
 
 public class DAHT {
-    int[] HT;
+    DA[] HT;
     int HTsize;
-    int tomb = 0;
 
     public DAHT(int size){
-        HT = new int[size];
+        HT = new DA[size];
         HTsize = size;
-        for(int i = tomb; i<HTsize;i++){
-            HT[i] = tomb;
-        }
     }
 
-    public void insert(int val){
-         if(HT[hash(val)] == tomb){
-             HT[hash(val)] = val;
-             return;
-         }
-         for(int i = hash(val); i<HTsize;i++){
-             if(HT[i] == tomb) {
-                 HT[i] = val;
-                 return;
-             }
-         }
-        //throw overflow error here
-        //Decided not to do chaining overflow+underflow
+    public void insert(DA data){
+        if(HT[data.getKey()]!=null){
+            System.out.println("Index already occupied");
+            throw new ArrayIndexOutOfBoundsException();
+        }
+        else HT[data.getKey()] = data;
+        //Overflow+underflow
     }
 
-    public void delete(int key){
-        HT[key] = tomb;
-/*
-        if(HT[hash(key)]==val){
-            HT[hash(key)] = tomb;
-            return;
+    public void delete(DA data){
+        if(data.getVal()==HT[data.getKey()].getVal()) HT[data.getKey()] = null;
+        else{
+            System.out.println("DA not at index");
+            throw new ArrayIndexOutOfBoundsException();
         }
-        int acc = 1;
-        for(int i = hash(key); i<HTsize;i++){
-            if(HT[i] == val) {
-                HT[i] = tomb;
-                return;
-            }
-        }
-
- */
-        //ret index does not exist
 
     }
 
-    public int search(int val){
-        int ret = hash(val);
-        if(HT[hash(val)] == val){
-            return ret;
-        }
-        for(int i = hash(val); i<HTsize;i++){
-            if(HT[i] == val) {
-                return i;
-            }
-        }
-        return -1;
-        //throw does not exist
+    public DA search(int key){
+        return HT[key];
+
+
     }
 
-//ensures that return val will be less than HTsize
-    private int hash(int key){
-        if(key<HTsize) return key;
-        int ret = key;
-        int acc = 1;
-        while(ret>=HTsize){
-            ret = ret%HTsize+acc;
-            acc++;
-        }
-        return ret;
+
+    public int hash(int key){
+        return key;
     }
 
-    public String tostring(){
-        return Arrays.toString(HT);
+    public void tostring(){
+        System.out.println(Arrays.toString(HT));
+    }
+    public DA[] getHT(){
+        return HT;
     }
 
 
